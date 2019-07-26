f5ff813109e09027aa63351455445acef715f145
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-26 10:20:32 -0700

    home page set

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require('sinatra/reloader')
 require('pry')
 require('./lib/learning')
 also_reload('lib/**/*.rb')
+
+get ('/') do
+  @learn = Dictionary.all
+  erb :default
+end
+
+get ('/home') do
+  @learn = Dictionary.all
+  erb :default
+end
--- lib/learn.rb
+++ lib/learn.rb
@@ -1,11 +1,12 @@
-class Word
+class Dictionary
   @@posts = []
   @@id_counter = 0
-  attr_reader :id
+  attr_reader :id, :word
   attr_accessor :definition
 
-  def initialize ()
+  def initialize (attributes)
     @@id_counter +=1
     @definition = attributes.fetch(:definition)
+    @word = attributes.fetch(:word)
     @id = @@id_counter
   end
